4769faec56c6b910bb43c79cffc35ce093def7a3
Thomas Altstidl <246-ul95odob@users.noreply.mad-srv.informatik.uni-erlangen.de>
2022-07-30 21:41:37 +0200

    Fix invalid bash range

--- scripts/train.sh
+++ scripts/train.sh
@@ -11,7 +11,7 @@ source .venv/bin/activate
 export PYTHONPATH=.
 export MLFLOW_EXPERIMENT_NAME=trial2
 export MLFLOW_TRACKING_URI=http://mad-vm-thomas.informatik.uni-erlangen.de/mlflow_siconv
-for seed in {1..$7}
+for seed in $(seq 1 $7)
 do
   srun python3 scripts/train.py --model $1 --data $2 --evaluation $3 --kernel-size $4 --interpolation $5 --lr $6 --seed $seed
 done
